84cd3d8b217b41c09332bbfc9614eeece33bdaec
Aron Roberts <aron@socrates.berkeley.edu>
2013-12-10 17:03:09 -0800

    CSPACE-6272: Vary tarball extraction behavior by platform.

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,26 +10,75 @@
 #
 # Sample Usage:
 #
-class cspace_tarball {
-  
-  exec { 'wget cspace-server-4.0.tar.gz':
-    command => 'wget ftp://source.collectionspace.org/pub/collectionspace/releases/4.0/cspace-server-4.0.tar.gz',
-    cwd     => '/usr/local/share',
-    creates => '/usr/local/share/cspace-server-4.0.tar.gz',
-    path    => [ '/usr/bin', '/usr/sbin' ]
-  }
 
-  exec { 'extract cspace-server-4.0.tar.gz':
-    command => 'tar -zxvof cspace-server-4.0.tar.gz',
-    cwd     => '/usr/local/share',
-    creates => '/usr/local/share/apache-tomcat-6.0.33',
-    path    => [ '/bin', '/usr/bin', '/usr/sbin' ],
-    require => Exec['wget cspace-server-4.0.tar.gz']
-  }
+include cspace_environment::execpaths
+include cspace_environment::osfamily
+
+class cspace_tarball ( $release_version = '4.0' ) {
+    
+  $os_family        = $cspace_environment::osfamily::os_family
+  $linux_exec_paths = $cspace_environment::execpaths::linux_default_exec_paths
+  $osx_exec_paths   = $cspace_environment::execpaths::osx_default_exec_paths
   
-  exec { 'chmod u+x *.sh':
-    cwd     => '/usr/local/share/apache-tomcat-6.0.33/bin',  
-    path    => [ '/bin', '/usr/bin', '/usr/sbin' ],  
-    require => Exec['extract cspace-server-4.0.tar.gz'],
+  case $os_family {
+    RedHat, Debian: {
+      $exec_paths = $linux_exec_paths
+    }
+    # Mac OS X
+    darwin: {
+      $exec_paths = $osx_exec_paths
+    }
+    # Microsoft Windows
+    windows: {
+    }
+    default: {
+    }
   }
+    
+  case $os_family {
+  
+    RedHat, Debian, darwin: {
+      
+      $distribution_filename  = "cspace-server-${release_version}.tar.gz"
+      $release_repository_dir = 'ftp://source.collectionspace.org/pub/collectionspace/releases'
+      $server_parent_dir      = '/usr/local/share'
+      $server_dir             = 'apache-tomcat-6.0.33'
+    
+      exec { 'Download CollectionSpace server distribution':
+        command => "wget ${release_repository_dir}/${release_version}/${distribution_filename}",
+        cwd     => $server_parent_dir,
+        creates => "${server_parent_dir}/${distribution_filename}",
+        path    => $exec_paths,
+      }
+
+      # FIXME: Verify whether the server directory already exists in
+      # the target location before extracting the distribution into
+      # that location.
+      
+      exec { 'Extract CollectionSpace server distribution':
+        command => 'tar -zxvof cspace-server-4.0.tar.gz',
+        cwd     => $server_parent_dir,
+        creates => "${server_parent_dir}/${server_dir}",
+        path    => $exec_paths,
+        require => Exec[ 'Download CollectionSpace server distribution' ]
+      }
+  
+      exec { 'Make Tomcat shell scripts executable':
+        command => 'chmod u+x *.sh',
+        cwd     => "${server_parent_dir}/${server_dir}/bin",  
+        path    => $exec_paths,
+        require => Exec[ 'Extract CollectionSpace server distribution' ],
+      }
+      
+    }
+    
+    # Microsoft Windows
+    windows: {
+    }
+  
+    default: {
+    }
+    
+  } # end case
+  
 }
--- tests/init.pp
+++ tests/init.pp
@@ -1,1 +1,4 @@
+include cspace_environment::execpaths
+include cspace_environment::osfamily
+
 class { 'cspace_tarball': } 
