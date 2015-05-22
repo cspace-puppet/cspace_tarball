aa20603cf8e4f80cd26ed8e09bb3c037ffa0a4c9
Aron Roberts <aron@socrates.berkeley.edu>
2015-05-22 13:30:29 -0700

    CSPACE-6376, CSPACE-6642: Change FTP site hostname in preparation for potential CSPACE-6642 deprecation of source.collectionspace.org.

--- manifests/init.pp
+++ manifests/init.pp
@@ -55,7 +55,7 @@ class cspace_tarball (
     RedHat, Debian, darwin: {
       
       $distribution_filename  = "cspace-server-${release_version}.tar.gz"
-      $release_repository_dir = 'ftp://source.collectionspace.org/pub/collectionspace/releases'
+      $release_repository_dir = 'ftp://nightly.collectionspace.org/pub/collectionspace/releases'
       $server_parent_dir      = '/usr/local/share'
           
       exec { 'Download CollectionSpace server distribution':
